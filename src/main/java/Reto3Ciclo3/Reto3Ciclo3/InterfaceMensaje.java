083a50b4a31188feb8a7c331ec264004ce98163d
NicolasTibambre <nicolas.tibambre.mt@correo.usa.edu.co>
2021-10-18 23:20:26 -0500

    first commit

---
+++ src/main/java/Reto3Ciclo3/Reto3Ciclo3/InterfaceMensaje.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto3Ciclo3.Reto3Ciclo3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author Nicolas
+ */
+public interface InterfaceMensaje extends CrudRepository<Mensaje,Integer>{
+    
+}
---
+++ src/main/java/Reto3Ciclo3/Reto3Ciclo3/Reto3Application.java
@@ -0,0 +1,18 @@
+
+package Reto3Ciclo3.Reto3Ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Nicolas
+ */
+@SpringBootApplication
+public class Reto3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3Application.class, args);
+	}
+
+}
